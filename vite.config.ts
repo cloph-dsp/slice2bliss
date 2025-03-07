b5479dc76b3def5100452c3ccc24c484a49c5f3a
cloph <128580843+cloph-dsp@users.noreply.github.com>
2025-03-07 09:02:55 +0000

    feat: update TypeScript configuration to disable emitting files; enhance Vite configuration with node polyfills; modify build scripts for cross-env usage

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,7 +19,12 @@ export default defineConfig(({ mode }) => {
   
   return {
     root: resolve(__dirname),
-    plugins: [react()],
+    plugins: [
+      react(),
+      nodePolyfills({
+        include: ['crypto']
+      })
+    ],
     base, // Set the correct base path
     resolve: {
       alias: {
